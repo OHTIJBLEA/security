8f840048502b3b9fe1c79abf2fe8e57f59f06fb7
OHTIJBLEA <95860743+OHTIJBLEA@users.noreply.github.com>
2022-07-22 16:29:11 +0400

    Update Role.java

--- src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
@@ -41,9 +41,4 @@ public class Role implements GrantedAuthority {
     public String getAuthority() {
         return getName();
     }
-
-    @Override
-    public String toString() {
-        return name;
-    }
 }
